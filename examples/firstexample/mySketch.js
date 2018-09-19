2be66e1edad5602b9331715d19265559ca9cc3eb
Alex Yixuan Xu <yx1137@nyu.edu>
2018-09-19 18:14:42 -0400

    edited first example

--- examples/firstexample/mySketch.js
+++ examples/firstexample/mySketch.js
@@ -7,6 +7,8 @@ var crazygrid = {rows: 50, cols: 50, backgroundcolor: 'Black', gridcolor: 'White
 
 var lukecrap = [{stuff: 50, things: 20}, {stuff: 55, things: 33}, {stuff: 33, things: 45}];
 var myData = [{"x":5, "y":10}, {"x":5, "y":10}, {"x":5, "y":10}, {"x":5, "y":10}, {"x":5, "y":10}, {"x":5, "y":10}, {"x":10, "y":10}, {"x":10, "y":10}, {"x":10, "y":10}, {"x":10, "y":10}, {"x":10, "y":10}, {"x":10, "y":10}, {"x":10, "y":10}, {"x":10, "y":10}, {"x":10, "y":10}, {"x":10, "y":10}, {"x":10, "y":10}, {"x":17, "y":10}, {"x":17, "y":10}, {"x":17, "y":10}, {"x":17, "y":10}, {"x":17, "y":10}];
+var boxData = [{"x":13, "y":"a"}, {"x":16, "y":"a"}, {"x":57, "y":"a"}, {"x":34, "y":"a"}, {"x":26, "y":"a"}, {"x":67, "y":"a"}, {"x":12, "y":"a"},
+ {"x":45, "y":"b"}, {"x":32, "y":"b"}, {"x":24, "y":"b"}, {"x":86, "y":"b"}, {"x":65, "y":"b"}, {"x":37, "y":"b"}, {"x":98, "y":"b"}];
 
 function preload() {
 	dfile = loadStrings('diamonds.json'); // load the big file
@@ -32,7 +34,9 @@ function draw() {
 	// PLOT COMMAND:
 	//foo.plot({type: 'point', data: lukecrap, xkey: 'stuff', ykey: 'things'});
 	//foo.plot({ data: mpg, xkey: 'year', ykey: 'displ' }); // default line
-	foo.plot({ type: 'density', data: myData, xkey: 'x', ykey: 'y', strokeweight: 5});
+	//foo.plot({ type: 'density', data: myData, xkey: 'x', ykey: 'y', strokeweight: 5});
+	//foo.plot({ type: 'box', data: economics, xkey: 'pop', ykey: 'unemploy', strokeweight: 5});
+	foo.plot({ type: 'box', data: boxData, xkey: 'x', ykey: 'y', strokeweight: 5});
 	//foo.plot({ type: 'area', data: economics, xkey: 'pop', ykey: 'unemploy', strokeweight: 5 }, mylook); // custom look
 	//foo.plot({ type: 'point', data: diamonds, xkey: 'carat', ykey: 'price', strokecolor: [255, 0, 0, 32], fillcolor: [128, 128, 255, 32] });
 	//foo.plot({ type: 'point', data: diamonds, xkey: 'carat', ykey: 'price', strokecolor: [255, 0, 0, 32], fillcolor: [128, 128, 255, 32] }, crazygrid);
