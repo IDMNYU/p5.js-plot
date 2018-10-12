fae34b7f97d108b2f4048ea31e75a052bd714c40
rev3rend <rev3rend@gmail.com>
2018-10-12 16:12:12 -0400

    added hover function

--- examples/firstexample/mySketch.js
+++ examples/firstexample/mySketch.js
@@ -20,27 +20,33 @@ function setup() {
 
 	diamonds = JSON.parse(dfile);
 
-	noLoop();
-}
 
-function draw() {
-	// mix and match:
+  // mix and match:
 
 	// CONSTRUCTOR:
 	//foo = new p5.Plot(); // easy
 		foo = new p5.Plot({ left: width*0.2, top: height*0.3, right: width*0.8, bottom: height*0.7 });
 	//foo = new p5.Plot({ left: 100, top: 100, right: 400, bottom: 200, backgroundcolor: 'lightBlue' });
 
+	//noLoop();
+}
+
+function draw() {
+  background(255);
+
 	// PLOT COMMAND:
+  //foo.plot({type: 'line', data: lukecrap, xkey: 'stuff', ykey: 'things'});
 	//foo.plot({type: 'point', data: lukecrap, xkey: 'stuff', ykey: 'things'});
 	//foo.plot({ data: mpg, xkey: 'year', ykey: 'displ' }); // default line
-	foo.plot({ type: 'density', data: economics, xkey: 'pop', strokeweight: 5});
+	//foo.plot({ type: 'density', data: economics, xkey: 'pop', strokeweight: 5});
 	//foo.plot({ type: 'box', data: economics, xkey: 'pop', strokeweight: 5});
   //foo.plot({ type: 'histogram', data: economics, xkey: 'pop', strokeweight: 5});
   //foo.plot({ type: 'box', data: mpg, xkey: 'displ', ykey: 'year', strokeweight: 5});
   //foo.plot({ type: 'box', data: diamonds, xkey: 'price', ykey: 'clarity', strokeweight: 1});
 	//foo.plot({ type: 'box', data: boxData, xkey: 'x', ykey: 'y', strokeweight: 5});
 	//foo.plot({ type: 'area', data: economics, xkey: 'pop', ykey: 'unemploy', strokeweight: 5 }, mylook); // custom look
-	//foo.plot({ type: 'point', data: diamonds, xkey: 'carat', ykey: 'price', strokecolor: [255, 0, 0, 32], fillcolor: [128, 128, 255, 32] });
+	foo.plot({ type: 'point', data: diamonds, xkey: 'carat', ykey: 'price', strokecolor: [255, 0, 0, 32], fillcolor: [128, 128, 255, 32] });
 	//foo.plot({ type: 'point', data: diamonds, xkey: 'carat', ykey: 'price', strokecolor: [255, 0, 0, 32], fillcolor: [128, 128, 255, 32] }, crazygrid);
+
+  foo.hover(mouseX, mouseY);
 }
