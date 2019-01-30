a9d3f9cfb67c39f0b926e73acb77b6d3f39a61ab
Alex Yixuan Xu <yx1137@nyu.edu>
2019-01-29 22:49:31 -0500

    fixing density plot

---
+++ examples/density_plot_example/mySketch.js
@@ -0,0 +1,26 @@
+var dfile, diamonds; // load from JSON
+
+var foo; // p5.Plot() object
+
+function preload() {
+	dfile = loadStrings('../../support/diamonds.json'); // load the big file
+}
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(255);
+
+	diamonds = JSON.parse(dfile);
+	// CONSTRUCTOR:
+	foo = new p5.Plot({ left: width*0.2, top: height*0.3, right: width*0.8, bottom: height*0.7 });
+
+  // PLOT COMMAND:
+  // DENSITY
+  foo.plot({ type: 'density', data: economics, xkey: 'pop', strokeweight: 3, pointdiameter: 5});
+}
+
+function draw() {
+  background(255);
+  foo.redraw();
+  //foo.hover(mouseX, mouseY, 'point');
+}
