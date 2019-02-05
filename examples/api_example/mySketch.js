d4201d33d6f408459cc1426c10d9ee9081b9860e
Alex Yixuan Xu <yx1137@nyu.edu>
2019-02-04 19:09:47 -0500

    added api example

---
+++ examples/api_example/mySketch.js
@@ -0,0 +1,26 @@
+let earthquakes;
+let myData = [];
+let foo;
+
+function preload(){
+  earthquakes = loadJSON('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson');
+}
+
+function setup(){
+  for (let i=0; i< earthquakes.features.length; i++){
+    let lat = earthquakes.features[i].geometry.coordinates[0];
+    let long = earthquakes.features[i].geometry.coordinates[1];
+    myData.push({lat, long});
+  }
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  // CONSTRUCTOR:
+  foo = new p5.Plot({left: width*0.2, top: height*0.3, right: width*0.8, bottom: height*0.7});
+  foo.plot({data: myData, type: 'point'});
+}
+
+function draw() {
+  background(255);
+  foo.redraw();
+	foo.hover(mouseX, mouseY, 'point');
+}
