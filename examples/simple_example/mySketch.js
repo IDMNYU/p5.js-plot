44b5151ebba90aebf0ae874fcd6253a30acf9784
rev3rend <rev3rend@gmail.com>
2019-10-16 12:15:57 -0400

    new stuff

--- examples/simple_example/mySketch.js
+++ examples/simple_example/mySketch.js
@@ -18,15 +18,21 @@ function setup(){
   createCanvas(800, 400);
   background(255);
   // CONSTRUCTOR:
-  foo = new p5.Plot();
+  foo = new p5.Plot({data: myData, type: ['line', 'point'], xkey: 'lat', ykey: 'long'});
 
 }
 
 function draw() {
-  background(255);
+  background(random()*255, random()*255, random()*255);
+  text(frameRate().toFixed(2), width-50, 50);
   x-=10;
   y+=10;
-  myData.push({lat:x,long:y});
-  foo.plot({data: myData, type: ['line', 'point'], xkey: 'lat', ykey: 'long'});
+  //myData.push({lat:x,long:y});
+  foo.plot();
   //console.log(foo.plotData);
+}
+
+function keyTyped() {
+  myData.push({lat:random(foo._xmin, foo._xmax),long:random(foo._ymin, foo._ymax)});
+  foo.data = myData;
 }
