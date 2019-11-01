347203c584605624b4b3ecd9e3ffa625c84d9bb1
Alex Yixuan Xu <yx1137@nyu.edu>
2019-11-01 16:19:04 -0400

    added svg html

--- examples/simple_example/mySketch.js
+++ examples/simple_example/mySketch.js
@@ -20,20 +20,23 @@ function setup(){
   createCanvas(800, 400);
   background(255);
   // CONSTRUCTOR:
-  foo = new p5.Plot({data: myData, type: ['line', 'point'], xkey: 'lat', ykey: 'long'});
+  foo = new p5.Plot({data: myData, type: ['point', 'line'], xkey: 'lat', ykey: 'long'});
   foo.plot();
-  mydiv = createDiv('').size(180,180);
-  mydiv.html('<svg width=\"180\" height=\"180\"><rect x=\"20\" y=\"20\" rx=\"20\" ry=\"20\" width=\"100\" height=\"100\" style=\"fill:lightgray; stroke:#1c87c9; stroke-width:4;\"/></svg>');
+  //mydiv = createDiv('').size(180,180);
+  //mydiv.html('<svg width=\"180\" height=\"180\"><rect x=\"20\" y=\"20\" rx=\"20\" ry=\"20\" width=\"100\" height=\"100\" style=\"fill:lightgray; stroke:#1c87c9; stroke-width:4;\"/></svg>');
 }
 
 function draw() {
+
+
   background(random()*255, random()*255, random()*255);
   text(frameRate().toFixed(2), width-50, 50);
   x-=10;
   y+=10;
+
   //myData.push({lat:x,long:y});
 
-  mydiv.position(mouseX, mouseY);
+  //mydiv.position(mouseX, mouseY);
   //console.log(foo.plotData);
 }
 
