da4084031ef59e52113ba00add72849ce325f4c2
Alex Yixuan Xu <yx1137@nyu.edu>
2019-12-03 18:38:35 -0500

    fixing svg drawn to canvas

--- examples/simple_example/mySketch.js
+++ examples/simple_example/mySketch.js
@@ -4,35 +4,37 @@ let foo;
 let x = -100;
 let y = 100;
 
-var mydiv;
+let myPlot;
 
 function preload(){
   earthquakes = loadJSON('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson');
+
 }
 
 function setup(){
+  createCanvas(800, 400);
+  background(255);
   for (let i=0; i< earthquakes.features.length; i++){
     let lat = earthquakes.features[i].geometry.coordinates[0];
     let long = earthquakes.features[i].geometry.coordinates[1];
     myData.push({lat, long});
   }
   //console.log(myData);
-  createCanvas(800, 400);
-  background(255);
+
   // CONSTRUCTOR:
   foo = new p5.Plot({data: myData, type: ['point', 'line'], xkey: 'lat', ykey: 'long'});
-  foo.plot();
-  //mydiv = createDiv('').size(180,180);
-  //mydiv.html('<svg width=\"180\" height=\"180\"><rect x=\"20\" y=\"20\" rx=\"20\" ry=\"20\" width=\"100\" height=\"100\" style=\"fill:lightgray; stroke:#1c87c9; stroke-width:4;\"/></svg>');
+  myPlot = foo.plot();
+  console.log(myPlot.width + ' '+myPlot.height);
 }
 
 function draw() {
-
-
-  background(random()*255, random()*255, random()*255);
+  //background(random()*255, random()*255, random()*255);
+  background(200);
   text(frameRate().toFixed(2), width-50, 50);
   x-=10;
   y+=10;
+
+  image(myPlot, 0, 0, myPlot.width, myPlot.height);
 
   //myData.push({lat:x,long:y});
 
